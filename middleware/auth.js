671344bd57237c714cb05028e18fbe6b522f188b
210352R <eshanmaduranga0329@gmail.com>
2023-09-27 12:41:03 +0530

    Try to create authenticatr route

--- middleware/auth.js
+++ middleware/auth.js
@@ -35,3 +35,57 @@ export default function auth(req, res, next) {
     });
   }
 }
+
+// export default function auth(req, res, next) {
+//   const authCookie = req.cookies.authcookie;
+//   console.log("Auth Cookie : ", authCookie);
+//   const token = authCookie;
+//   console.log("Token : ", token);
+
+//   if (token == null) {
+//     res.send(401).json({
+//       message: "Not authorized --- ",
+//     });
+//   }
+//   jwt.verify(token, "123456", (err, user) => {
+//     if (err) {
+//       res.status(403).json({
+//         message: "Prohibidden",
+//       });
+//     }
+//     console.log("Task successfull using cookies --- -----------");
+//     req.user = user; //req ekata user ekenek danawa ---
+//     next();
+//   });
+// }
+
+// next();
+//get authcookie from request
+// if (
+//   req?.headers.authorization &&
+//   req?.headers.authorization.startsWith("Bearer")
+// ) {
+//   //meke [0] element eka thamai "Bearer" [1] ta thamai token eka denne
+//   const token = req?.headers.authorization.split(" ")[1];
+//   console.log(token);
+//   if (token == null) {
+//     res.send(401).json({
+//       message: "Not authorized --- ",
+//     });
+//   }
+//   jwt.verify(token, "123456", (err, user) => {
+//     if (err) {
+//       res.status(403).json({
+//         message: "Prohibidden",
+//       });
+//     }
+//     console.log("Wade goda -----------");
+//     req.user = user; //req ekata user ekenek danawa ---
+//     next();
+//   });
+// } else {
+//   res.status(401).json({
+//     message: "Access Denied",
+//   });
+// }
+//}
